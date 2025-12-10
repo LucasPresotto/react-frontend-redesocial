ffbd01a1a8ec535958d21aa4aacea98dcbec5230
lucas <lucaspresottocosta@gmail.com>
2025-12-09 22:47:13 -0300

    ajustes finais

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const BACKEND_URL = "https://desenvolvimento-web-2025-5vis.onrender.com";
 
 const csp = [
   "default-src 'none'",
-  "script-src 'self' 'unsafe-inline'",
+  "script-src 'self'",
   "style-src 'self' 'unsafe-inline'",
   `img-src 'self' data: http://localhost:3000 ${BACKEND_URL}/uploads https://placehold.co https://picsum.photos`,
   "font-src 'self'",
